2a2c655244663030b966f8f4ecd70221b7a341e6
pedro-drosa <pedro_drosa@hotmail.com>
2019-12-10 08:14:30 -0300

    route creation[store]

--- app/routes.php
+++ app/routes.php
@@ -7,4 +7,5 @@ return function (App $app)
 {
     $app->get('/users', UserController::class.":index");
     $app->get('/users/{id}', UserController::class.":show");
+    $app->post('/users', UserController::class.":store");
 };
--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controllers;
 
+use Illuminate\Database\QueryException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Models\User as User;
@@ -18,12 +19,32 @@ class UserController
     {
         $user = User::find($args['id']);
         if ($user) {
-            return $res->withJson($user);
+            return $res->withJson([$user]);
         }else{
             return $res->withJson(["message"=>"Nothing found"], 404);
         }
     }
-    public function store(){}
+    public function store(Request $req, Response $res, array $args)
+    {
+        $body = $req->getParsedBody();
+        if (empty($body['user_name'])) {
+            return $res->withJson(["error"=>"name"], 400);
+        }
+        if (empty($body['user_email'])) {
+            return $res->withJson(["error"=>"email"], 400);
+        }
+        if (empty($body['user_password'])) {
+            return $res->withJson(["error"=>"password"], 400);
+        }
+        try {
+            $id = User::insertGetId($body);
+            $newUser = User::find($id);
+            return $res->withJson($newUser, 201);
+        } catch (QueryException $e) {
+            return $res->withJson(["error"=>"User alread exists"], 400);
+        }
+        return $res->withJson(["error"=>"Registration Failed"], 400);
+    }
     public function update(){}
     public function destroy(){}
 }
